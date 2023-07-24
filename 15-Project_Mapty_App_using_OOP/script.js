fb65980534373852365876e6786b1d5b5f9d8ec9
rajneeshsingh18 <rajneeshsingh9448@gmail.com>
2023-07-24 23:01:19 +0530

    Class architecture implementation

--- 15-Project_Mapty_App_using_OOP/script.js
+++ 15-Project_Mapty_App_using_OOP/script.js
@@ -12,44 +12,109 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 
+
+
 //Project Planning
 
 // 1. User Stories --> 2. Features --> 3.FlowChart --> 4. Architecture --> Development step
 
 
-//Using the Geoloaction API
+//Class architecture implementation
+class App {
+    #map;
+    #mapEvent;
 
-if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change',this._toggleElevationField)
+    }
 
-            console.log(position);
+    _getPosition() {
+        //Using the Geoloaction API
+        if (navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),
+                function () {
+                    alert('Could not get your position')
+                });
+    }
 
-            const { latitude } = position.coords;
-            const { longitude } = position.coords;
-            console.log(position.coords);
+    _loadMap(position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(position.coords);
 
-            console.log(latitude, longitude);
+        console.log(latitude, longitude);
 
-            console.log(` https://www.google.com/maps/@${latitude},${longitude}`);
+        console.log(` https://www.google.com/maps/@${latitude},${longitude}`);
+
+        //Display a Map Using Leaflet Library
+
+        const coords = [latitude, longitude]
+
+        // https://leafletjs.com/index.html
+
+        console.log(this);
+        this.#map = L.map('map').setView(coords, 13);
+        // console.log(map);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+
+        //Handling clicks on map
+        this.#map.on('click', this._showForm.bind(this))
+    }
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+     }
+
+    _newWorkout(e) {
+        e.preventDefault()
+
+        console.log(this);
+
+        //CLEAR input fields
+        inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = " ";
+
+        //Display marker
+        const { lat, lng } = this.#mapEvent.latlng
+        L.marker([lat, lng])
+            .addTo(this.#map)
+            .bindPopup(L.popup({
+
+                //see docs in leaflet library
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout')
+            .openPopup();
+    }
+}
 
 
-            const coords = [latitude , longitude]
+const app = new App();
 
 
-            const map = L.map('map').setView(coords, 13);
-
-            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-
-            L.marker(coords).addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-                .openPopup();
-
-        }, function () {
-            alert('Could not get your position')
-        })
 
 
-//Display a Map Using Leaflet Library
+
+
+
+
+
+
+
+
+
